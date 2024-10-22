3a69a2ca6c1a16e40c18928a6b689ebee12b5ccf
salsama1 <salehh2003@gmail.com>
2024-10-22 22:10:52 +0300

    fin

--- ocr-backend/main.py
+++ ocr-backend/main.py
@@ -62,5 +62,4 @@ async def upload(file: UploadFile = File(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.getenv("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
